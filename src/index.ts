969db0ec16357cfd324f6b52313e6916b6f5eacd
Margarita <Margarita>
2023-10-14 12:42:07 +0300

    fix: on process args

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {Game} from "./classes/Game"
 
-const moves = ['rock', 'Spock', 'paper', 'lizard', 'scissors'] //process.argv.slice(2); //
+const moves = process.argv.slice(2);
 
 const game = new Game(moves)
 game.start()
